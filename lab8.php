57fb2d1c5be575ccce1456937c6be56db54adb52
CMarmot <corleneankrum@gmail.com>
2019-02-23 17:30:09 -0800

    nav bar and styling

--- lab8.php
+++ lab8.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
 <html>
 <head>
 	<title>Temperature Converter</title>
@@ -8,77 +10,44 @@
 	<!-- add link to bootstrap js -->
 </head>
 <body>
+<div class =container-fluid>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class ="blue">
+
+   
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://en.wikipedia.org/wiki/Anders_Celsius">Mr. Celsius</a>
+            </li>
+			<li class="nav-item">
+                <a class="nav-link" href="https://imgur.com/gallery/3ZidINK">Celsius vs Fahrenheit</a>
+            </li>
+			<li class="nav-item">
+                <a class="nav-link" href="https://www.famousbirthdays.com/names/kelvin.html">Kelvins</a>
+            </li>
+      
+        </ul>
+        
+    </div>
+</nav>
 	
 <?php 
 // function to calculate converted temperature
-function convertTemp($temp,$unit1,$unit2){
-	switch ($unit1) {
-		case 'celsius':
 
-			if ($unit1 == "celsius" and $unit2 == "farenheit"){
-				$F = $_POST['originaltemp'] * 9/5 + 32;
-				return $F;
-			} // end if
+include 'inc/functions.php';
 
-			if ($unit1 == "celsius" and $unit2 == "kelvin"){
-				$K = $_POST['originaltemp'] + 273.15;
-				return $K;
-			} // end if
-
-		case 'farenheit':
-			if ($unit1 == "farenheit" and $unit2 == "celsius"){
-				$C = ($_POST['originaltemp'] - 32 ) * 5/9;
-				return $C;
-			} // end if
-
-			if ($unit1 == "farenheit" and $unit2 == "kelvin"){
-				$K = ($_POST['originaltemp'] + 459.67) * 5/9;
-				return $K;
-			} // end if	
-
-		case 'kelvin':
-			if ($unit1 == "kelvin" and $unit2 == "celsius"){
-				$C = $_POST['originaltemp'] - 273.15;
-				return $C;
-			} // end if
-
-			if ($unit1 == "kelvin" and $unit2 == "farenheit"){
-				$F = $_POST['originaltemp'] * 9/5 - 459.67;
-				return $F;
-			} // end if
-
-		default:
-			if ($unit1 == $unit2){
-				return $temp;
-			}
-	} // end switch
-
-} // end function
-
-#CHECK TO SEE IF FORM WAS SUBMITTED
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-	$originalTemperature = $_POST['originaltemp'];
-	$originalUnit= $_POST['originalunit'];
-	$conversionUnit = $_POST['conversionunit'];
-	$convertedTemp = convertTemp($originalTemperature,$originalUnit,$conversionUnit);
-} // end if
-
-if (isset($_POST['originalunit'])){
-	$originalUnit = $_POST['originalunit'];
-} else {
-	// looks like the form wasn't being posted
-	$originalUnit = "";
-} // end if
-
-if (isset($_POST['conversionunit'])){
-	$conversionUnit = $_POST['conversionunit'];
-} else {
-  	// looks like the form wasn't being posted
-	$conversionUnit = "";
-} // end if
 ?>
 
-<div id="wrapper">
+<div class="homedescription">
 	<h1>Temperature Converter</h1>
 	<h4>CTEC 127 - PHP with SQL 1</h4>
 	<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
@@ -107,6 +76,9 @@ if (isset($_POST['conversionunit'])){
 		</div>
 		<input type="submit" value="Convert"/>   
 	</form>
-</div><!-- end wrapper div-->
+</div><!-- end homedescription div-->
 </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
